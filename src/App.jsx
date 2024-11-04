acdc5b2adb94785c6723497967fe986ea33b0eba
dinh van linh <dinhvanlinh06052002@gmail.com>
2024-11-04 21:39:21 +0700

    myproject

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SampleHome from "./samplePages/SampleHome";
+
+function App() {
+  return (
+    <div>
+      <SampleHome />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+    return (
+        <div className="bg-blue-500 text-white p-4 text-center mt-auto">
+            <div>&copy; Copyright | All Rights Reserved</div>
+        </div>
+    )
+}
+
+export default footer
